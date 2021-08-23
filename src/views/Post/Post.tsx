91458ad09796b08081a815ff6f00435800e873d3
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-23 16:58:18 +0200

    Finished UI for posts

--- src/views/Post/Post.tsx
+++ src/views/Post/Post.tsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'graphql-hooks';
 import Loading from 'components/molecules/Loading/Loading';
+import styled from 'styled-components';
+import { themeProps } from 'assets/css/theme';
+import { Title } from 'components/atoms/Title/Title';
+
+export const Image = styled.img`
+  width: 100%;
+  border-radius: 20px;
+  box-shadow: ${({ theme }: themeProps) => theme.shadows.primary};
+`;
+
+export const StyledTitle = styled(Title)`
+  margin-top: 15px;
+  text-align: center;
+  font-size: 35px;
+`;
+export const Description = styled.p`
+  text-align: justify;
+  font-size: 20px;
+  letter-spacing: 1px;
+`;
+export const Author = styled.div`
+  width: fit-content;
+  margin: 0 auto;
+  opacity: 0.7;
+`;
 
 interface postProps {
   id: string;
@@ -9,7 +34,7 @@ interface postProps {
   description: string;
   createdBy: {
     name: string;
-    publishedAt: string;
+    createdAt: string;
   };
   image: {
     url: string;
@@ -23,7 +48,7 @@ const Post = (): JSX.Element => {
       description
       createdBy {
         name
-        publishedAt
+        createdAt
       }
       title
       id
@@ -35,19 +60,29 @@ const Post = (): JSX.Element => {
   `;
   const { data: postInfo, loading: postLoading } = useQuery(query);
 
+  const [postTime, setPostTime] = useState('Loading...');
+  useEffect(() => {
+    if (!postLoading) {
+      const UNIX = Date.parse(postInfo.articles[0].createdBy.createdAt);
+      const time = new Date(UNIX);
+      const parsed = `${time.getDay()}.${time.getMonth()}.${time.getFullYear()} - ${time.getHours()}:${time.getMinutes()}`;
+      setPostTime(parsed);
+    }
+  }, [postLoading]);
+
   return (
     <div>
       {postLoading ? (
         <Loading />
       ) : (
-        postInfo.articles.map(({ title, description, createdBy: { name, publishedAt }, id, image: { url } }: postProps) => (
+        postInfo.articles.map(({ title, description, createdBy: { name }, id, image: { url } }: postProps) => (
           <div key={id}>
-            <img src={url} alt={title} />
-            <h1>{title}</h1>
-            <p>{description}</p>
-            <p>
-              Autor - {name} | {publishedAt}
-            </p>
+            <Image src={url} alt={title} />
+            <StyledTitle>{title}</StyledTitle>
+            <Description>{description}</Description>
+            <Author>
+              {name} | {postTime}
+            </Author>
           </div>
         ))
       )}
