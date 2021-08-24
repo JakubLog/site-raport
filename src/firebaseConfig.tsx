d4a62f0523ca3e9e790a3d961c5559213cd36268
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-24 09:04:25 +0200

    Add firebase to project
    Added firebase configuration for project.
    FirebaseConfig.tsx include exported auth variable.
    
    Installed packages:
    * firebase

---
+++ src/firebaseConfig.tsx
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPWQj25kxbtWIitG-cT_cqwNR-PfONDIY',
+  authDomain: 'raport-82ead.firebaseapp.com',
+  projectId: 'raport-82ead',
+  storageBucket: 'raport-82ead.appspot.com',
+  messagingSenderId: '336466001102',
+  appId: '1:336466001102:web:647f85ae62ed587b9ea49f'
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
