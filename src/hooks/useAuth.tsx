f8fe32ed11d8bd3a08f9cff943aa7608aec464dd
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-24 09:05:36 +0200

    Add useAuth hook
    UseAuth hook makes it possible to authorizate users.

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,70 @@
+import firebase from 'firebase';
+import { auth } from 'firebaseConfig';
+import React, { createContext, useState, useEffect, useContext } from 'react';
+import { useError } from './useError';
+
+// Interfaces of types
+interface props {
+  children: React.ReactNode;
+}
+
+interface authContextInterface {
+  authUser: Partial<firebase.User> | null;
+  login: (data: { email: string; password: string }) => Promise<firebase.auth.UserCredential>;
+  loginWithGoogle: () => Promise<void>;
+  register: (data: { newemail: string; newpassword: string }) => Promise<firebase.auth.UserCredential>;
+  logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<Partial<authContextInterface>>({});
+
+const AuthProvider = ({ children }: props): JSX.Element => {
+  // States
+  const [currentUser, setCurrentUser] = useState<Partial<firebase.User> | null>(null);
+  const [isLoading, setLoadingState] = useState<boolean>(true);
+
+  // Firebase auth methods
+  const login = ({ email, password }: { email: string; password: string }) => auth.signInWithEmailAndPassword(email, password);
+  const loginWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return auth.signInWithRedirect(provider);
+  };
+  const register = ({ newemail: email, newpassword: password }: { newemail: string; newpassword: string }) =>
+    auth.createUserWithEmailAndPassword(email, password);
+  const logout = () => auth.signOut();
+
+  // User mounting and demounting
+  useEffect(() => {
+    const unregister = auth.onAuthStateChanged((user) => setCurrentUser(user));
+    setLoadingState(false);
+
+    return unregister;
+  }, []);
+
+  // Object with context values
+  const value: authContextInterface = {
+    authUser: currentUser,
+    login,
+    loginWithGoogle,
+    register,
+    logout
+  };
+
+  return <AuthContext.Provider value={value}>{!isLoading && children}</AuthContext.Provider>;
+};
+
+export const useAuth = (): Partial<authContextInterface> | void => {
+  const { dispatchError } = useError();
+
+  const auth = useContext(AuthContext);
+
+  if (!auth) {
+    const errMessage = 'Auth is not available now, we so sorry! Please try again later.';
+    dispatchError ? dispatchError(errMessage) : console.error(errMessage);
+    return;
+  }
+
+  return auth;
+};
+
+export default AuthProvider;
