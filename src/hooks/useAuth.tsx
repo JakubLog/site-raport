c7bda6bd936644d98b77c5f6b205e9c8fe96d611
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-08-30 11:29:31 +0200

    Add registration mechanics - social media, basic

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,6 +1,6 @@
 import Loading from 'components/molecules/Loading/Loading';
 import firebase from 'firebase';
-import { auth } from 'firebaseConfig';
+import { auth, db } from 'firebaseConfig';
 import React, { createContext, useState, useEffect, useContext } from 'react';
 import { useError } from './useError';
 
@@ -13,8 +13,8 @@ interface authContextInterface {
   authUser: Partial<firebase.User> | null;
   changeEmail: (email: string) => Promise<void> | Error;
   login: (data: { email: string; password: string }) => Promise<firebase.auth.UserCredential>;
-  loginWithGoogle: () => Promise<void>;
-  register: (data: { newEmail: string; newPassword: string; confirmPassword: string }) => Promise<firebase.auth.UserCredential>;
+  loginWithGoogle: () => Promise<void | false>;
+  register: (data: { newEmail: string; newPassword: string; confirmPassword: string }) => Promise<void | Error>;
   logout: () => Promise<void>;
 }
 
@@ -24,15 +24,56 @@ const AuthProvider = ({ children }: props): JSX.Element => {
   // States
   const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
   const [isLoading, setLoadingState] = useState<boolean>(true);
+  const { dispatchError } = useError();
+
+  // Firestore creating account method
+  const createFirestoreAccount = async (email: string) => {
+    if (!email) throw new Error('Email does not exist in register protocol!');
+    await db
+      .collection('users')
+      .doc()
+      .set({
+        email: email,
+        name: email.split('@')[0],
+        bio: 'This is your default BIO. If you want, change this text. BIO is text about you and your profile.'
+      });
+  };
 
   // Firebase auth methods
   const login = ({ email, password }: { email: string; password: string }) => auth.signInWithEmailAndPassword(email, password);
-  const loginWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    return auth.signInWithRedirect(provider);
+  const loginWithGoogle = async () => {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('email');
+      await auth.signInWithPopup(provider).then(async (result) => {
+        if (result.user !== null) {
+          let doesExist;
+          await db
+            .collection('users')
+            .where('email', '==', result.user.email)
+            .get()
+            .then((snapshots) => snapshots.forEach((snapshot) => (doesExist = snapshot.get('name'))));
+
+          if (doesExist !== undefined) {
+            return false;
+          } else {
+            if (result?.user.email) createFirestoreAccount(result.user.email);
+            else throw new Error('Result.user.email does not exist in register protocol!');
+          }
+        } else throw new Error('Result.user does not exist in register protocol!');
+      });
+    } catch (e) {
+      // console.error(e);
+      if (e.message === 'The popup has been closed by the user before finalizing the operation.') return false;
+      const errMessage = "Sorry, we can't create your account. Please contact with administration";
+      dispatchError ? dispatchError(errMessage) : console.error(errMessage);
+      auth.signOut();
+    }
   };
-  const register = ({ newEmail: email, newPassword: password }: { newEmail: string; newPassword: string; confirmPassword: string }) =>
-    auth.createUserWithEmailAndPassword(email, password);
+  const register = async ({ newEmail: email, newPassword: password }: { newEmail: string; newPassword: string; confirmPassword: string }) => {
+    await auth.createUserWithEmailAndPassword(email, password);
+    await createFirestoreAccount(email);
+  };
   const logout = () => auth.signOut();
   const changeEmail = (email: string) => (currentUser ? currentUser.updateEmail(email) : new Error("currentUser doesn't exist in auth hook!"));
 
