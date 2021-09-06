2c938b9b305bff7f96c6062ad87e585e1e63d2c9
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-09-06 16:51:16 +0200

    Add mobile version for favorite button

--- src/views/Post/Post.styles.tsx
+++ src/views/Post/Post.styles.tsx
@@ -64,9 +64,9 @@ export const Favorite = styled.div<{ isActive?: boolean }>`
   position: absolute;
   top: 0;
   right: 0;
-  width: 40px;
+  width: 100%;
   height: 40px;
-  border-radius: 50%;
+  border-radius: 25px 25px 0 0;
   background-color: ${({ theme }: themeProps) => theme.colors.purpleDark};
   box-shadow: ${({ theme }: themeProps) => theme.shadows.primary};
   &:hover {
@@ -80,9 +80,17 @@ export const Favorite = styled.div<{ isActive?: boolean }>`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 20px;
+    width: 90%;
     height: 20px;
-    border-radius: 50%;
+    border-radius: 10px;
     background-color: ${({ theme }: themeProps) => theme.colors.purple};
+  }
+  @media (min-width: 768px) {
+    width: 40px;
+    border-radius: 50%;
+    &::after {
+      border-radius: 50%;
+      width: 50%;
+    }
   }
 `;
