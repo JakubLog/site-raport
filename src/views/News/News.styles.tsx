bf6b2d1028c94e33c5ac99378ee7dd21ca89fa3f
JaFe <kontakt.jakubfedoszczak@gmail.com>
2021-11-04 05:03:32 +0100

    Static

--- src/views/News/News.styles.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  @media (min-width: 768px) {
-    display: grid;
-    grid-template-columns: auto 400px;
-    grid-gap: 30px;
-  }
-`;
